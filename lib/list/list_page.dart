fb5ad1b666b89b3d80abaeade532d4ec4deb1db0
dlworua <leegaegyum@gmail.com>
2025-04-03 19:03:25 +0900

    feat: listpage appbar 완성

---
+++ lib/list/list_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  const ListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          '플라워링',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Icon(Icons.shopping_cart_outlined, size: 30),
+          SizedBox(width: 25),
+        ],
+      ),
+    );
+  }
+}
