f3331d3b078cf7a286a047945cb508c0157d4b44
dlworua <leegaegyum@gmail.com>
2025-04-03 19:03:25 +0900

    feat: listpage appbar 완성

--- lib/page/detail/detail_page.dart
+++ lib/page/detail/detail_page.dart
@@ -4,6 +4,7 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      ///뒤로 가기 버튼
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
