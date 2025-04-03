341b40f3d05d79d7d6b9c5a701b89ec7653aa2b8
dlworua <leegaegyum@gmail.com>
2025-04-03 19:18:01 +0900

    docs: title 사이즈 원상복구

--- lib/list/list_page.dart
+++ lib/list/list_page.dart
@@ -8,11 +8,8 @@ class ListPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('플라워링', style: TextStyle(fontSize: 25)),
-        actions: [
-          Icon(Icons.shopping_cart_outlined, size: 30),
-          SizedBox(width: 25),
-        ],
+        title: const Text('플라워링'),
+        actions: [Icon(Icons.shopping_cart_outlined), SizedBox(width: 25)],
       ),
     );
   }
